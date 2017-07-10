2d05a3024bb86a0da11dbb3f909c28786cf4e14b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-07-10 16:35:44 +0300

    Fix storage to use name not the unreliable hostname.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 systemd --version
 
 mkdir -p /root/.ssh
-cp /id_rsa.pub /root/.ssh/authorized_keys
+cp /host-id_rsa.pub /root/.ssh/authorized_keys
 chown root -R /root/.ssh
 chmod go-wx -R /root/.ssh
-mkdir -p /storage/$(hostname)
-ln -sf /storage/$(hostname) /var/lib/docker
+
+storage_path=/storage$(DOCKER_HOST=unix:///host-docker.sock docker inspect -f '{{ .Name }}' $(hostname))
+mkdir -p $storage_path
+ln -sf $storage_path /var/lib/docker
 exec "$@"
